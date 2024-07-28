6ff3a7d8dd86cba5716c4eb75865265567ba2a81
Shu Yang <shu@Shus-MBP.lan>
2024-07-28 12:27:00 -0700

    set debug value

--- django_tangobookings/settings.py
+++ django_tangobookings/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.enviorn.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['tangobookings-heroku-1fd475c5f0a1.herokuapp.com']
 
